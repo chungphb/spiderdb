a60586c546b1bf19ba66077e39c5dc71bfea1033
Pham Huu Bao Chung <phamhuubaochung@gmail.com>
2021-04-16 22:23:16 +0700

    Create project

---
+++ src/core/spiderdb.cpp
@@ -0,0 +1,7 @@
+#include <spiderdb/core/spiderdb.h>
+
+namespace spiderdb {
+    void run() {
+        std::cout << "SpiderDB" << std::endl;
+    }
+}
---
+++ test/test_spiderdb.cpp
@@ -0,0 +1,6 @@
+#include <spiderdb/core/spiderdb.h>
+
+int main(int argc, char** argv) {
+    using namespace spiderdb;
+    run();
+}
