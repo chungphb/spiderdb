dc17c8a9c5344fcffe342b0804fa7f5cfc92a359
Pham Huu Bao Chung <phamhuubaochung@gmail.com>
2021-04-25 02:13:43 +0700

    Test LRU Cache: Get

--- tests/unit/cache_test.cpp
+++ tests/unit/cache_test.cpp
@@ -24,7 +24,7 @@ struct hash {
 };
 
 template <typename key_t, typename value_t>
-item_list_t<key_t, value_t> generate_consecutive_access_pattern(size_t n_items, key_t from) {
+item_list_t<key_t, value_t> generate_consecutive_data(size_t n_items, key_t from) {
     static_assert(std::is_same_v<key_t, int> && std::is_constructible_v<value_t, key_t>);
     item_list_t<key_t, value_t> list;
     for (size_t i = 0; i < n_items; ++i) {
@@ -36,7 +36,7 @@ item_list_t<key_t, value_t> generate_consecutive_access_pattern(size_t n_items, 
 }
 
 template <typename key_t, typename value_t>
-item_list_t<key_t, value_t> generate_random_access_pattern(size_t n_items, key_t min, key_t max) {
+item_list_t<key_t, value_t> generate_random_data(size_t n_items, key_t min, key_t max) {
     static_assert(std::is_same_v<key_t, int> && std::is_constructible_v<value_t, key_t>);
     assert(min <= max);
     item_list_t<key_t, value_t> list;
@@ -121,17 +121,17 @@ SPIDERDB_TEST_SUITE(cache_test)
 
 SPIDERDB_TEST_CASE(test_put_consecutive_data_with_number_of_unique_items_less_than_or_qual_to_cache_capacity) {
     // Create cache
-    size_t capacity = 256;
-    auto cache_history = seastar::make_lw_shared<std::list<std::pair<int, int>>>();
+    const size_t capacity = 256;
+    const auto cache_history = seastar::make_lw_shared<std::list<std::pair<int, int>>>();
     auto evictor = [cache_history](const std::pair<int, int>& evicted_item) -> seastar::future<> {
         cache_history->push_front(evicted_item);
         return seastar::now();
     };
-    auto cache = seastar::make_lw_shared<spiderdb::cache<int, int>>(capacity, std::move(evictor));
+    const auto cache = seastar::make_lw_shared<spiderdb::cache<int, int>>(capacity, std::move(evictor));
 
     // Generate access pattern
-    auto n_unique_items = 64;
-    auto access_pattern = seastar::make_lw_shared(generate_consecutive_access_pattern<int, int>(n_unique_items, 0));
+    const auto n_unique_items = 64;
+    const auto access_pattern = seastar::make_lw_shared(generate_consecutive_data<int, int>(n_unique_items, 0));
     print("Access pattern", *access_pattern);
 
     // Put items into cache
@@ -141,7 +141,7 @@ SPIDERDB_TEST_CASE(test_put_consecutive_data_with_number_of_unique_items_less_th
         return cache->put(key, value);
     }).then([cache, access_pattern, cache_history] {
         // Check result
-        auto expected_items = reverse(*access_pattern);
+        const auto& expected_items = reverse(*access_pattern);
         print("Cache items (actual)", cache->get_all_items());
         print("Cache items (expected)", expected_items);
         SPIDERDB_REQUIRE((is_equal<int, int>(cache->get_all_items(), expected_items)));
@@ -151,17 +151,17 @@ SPIDERDB_TEST_CASE(test_put_consecutive_data_with_number_of_unique_items_less_th
 
 SPIDERDB_TEST_CASE(test_put_consecutive_data_with_number_of_unique_items_greater_than_cache_capacity) {
     // Create cache
-    size_t capacity = 256;
-    auto cache_history = seastar::make_lw_shared<std::list<std::pair<int, int>>>();
+    const size_t capacity = 256;
+    const auto cache_history = seastar::make_lw_shared<std::list<std::pair<int, int>>>();
     auto evictor = [cache_history](const std::pair<int, int>& evicted_item) -> seastar::future<> {
         cache_history->push_front(evicted_item);
         return seastar::now();
     };
-    auto cache = seastar::make_lw_shared<spiderdb::cache<int, int>>(capacity, std::move(evictor));
+    const auto cache = seastar::make_lw_shared<spiderdb::cache<int, int>>(capacity, std::move(evictor));
 
     // Generate access pattern
-    auto n_unique_items = 512;
-    auto access_pattern = seastar::make_lw_shared(generate_consecutive_access_pattern<int, int>(n_unique_items, 0));
+    const auto n_unique_items = 512;
+    const auto access_pattern = seastar::make_lw_shared(generate_consecutive_data<int, int>(n_unique_items, 0));
     print("Access pattern", *access_pattern);
 
     // Put items into cache
@@ -171,28 +171,28 @@ SPIDERDB_TEST_CASE(test_put_consecutive_data_with_number_of_unique_items_greater
         return cache->put(key, value);
     }).then([cache, access_pattern, cache_history, capacity, n_unique_items] {
         // Check result
-        auto expected_items = get_first_n_items(reverse(*access_pattern), capacity);
+        const auto& expected_items = get_first_n_items(reverse(*access_pattern), capacity);
         print("Cache items (actual)", cache->get_all_items());
         print("Cache items (expected)", expected_items);
         SPIDERDB_REQUIRE((is_equal<int, int>(cache->get_all_items(), expected_items)));
-        auto expected_cache_history = reverse(get_first_n_items(*access_pattern, n_unique_items - capacity));
+        const auto& expected_cache_history = reverse(get_first_n_items(*access_pattern, n_unique_items - capacity));
         SPIDERDB_REQUIRE((is_equal<int, int>(*cache_history, expected_cache_history)));
     });
 }
 
 SPIDERDB_TEST_CASE(test_put_random_data_with_number_of_unique_items_less_than_or_qual_to_cache_capacity) {
     // Create cache
-    size_t capacity = 256;
-    auto cache_history = seastar::make_lw_shared<std::list<std::pair<int, int>>>();
+    const size_t capacity = 256;
+    const auto cache_history = seastar::make_lw_shared<std::list<std::pair<int, int>>>();
     auto evictor = [cache_history](const std::pair<int, int>& evicted_item) -> seastar::future<> {
         cache_history->push_front(evicted_item);
         return seastar::now();
     };
-    auto cache = seastar::make_lw_shared<spiderdb::cache<int, int>>(capacity, std::move(evictor));
+    const auto cache = seastar::make_lw_shared<spiderdb::cache<int, int>>(capacity, std::move(evictor));
 
     // Generate access pattern
-    auto n_unique_items = 64;
-    auto access_pattern = seastar::make_lw_shared(generate_random_access_pattern<int, int>(capacity, 0, n_unique_items));
+    const auto n_unique_items = 64; // Just an upper bound of the actual number of unique items
+    const auto access_pattern = seastar::make_lw_shared(generate_random_data<int, int>(256, 0, n_unique_items));
     print("Access pattern", *access_pattern);
 
     // Put items into cache
@@ -202,7 +202,7 @@ SPIDERDB_TEST_CASE(test_put_random_data_with_number_of_unique_items_less_than_or
         return cache->put(key, value);
     }).then([cache, access_pattern, cache_history] {
         // Check result
-        auto expected_items = remove_duplication(reverse(*access_pattern));
+        const auto& expected_items = remove_duplication(reverse(*access_pattern));
         print("Cache items (actual)", cache->get_all_items());
         print("Cache items (expected)", expected_items);
         SPIDERDB_REQUIRE((is_equal<int, int>(cache->get_all_items(), expected_items)));
@@ -212,20 +212,22 @@ SPIDERDB_TEST_CASE(test_put_random_data_with_number_of_unique_items_less_than_or
 
 SPIDERDB_TEST_CASE(test_put_random_data_with_number_of_unique_items_greater_than_cache_capacity) {
     // Create cache
-    size_t capacity = 256;
-    auto cache_history = seastar::make_lw_shared<std::list<std::pair<int, int>>>();
+    const size_t capacity = 256;
+    const auto cache_history = seastar::make_lw_shared<std::list<std::pair<int, int>>>();
     auto evictor = [cache_history](const std::pair<int, int>& evicted_item) -> seastar::future<> {
         cache_history->push_front(evicted_item);
         return seastar::now();
     };
-    auto cache = seastar::make_lw_shared<spiderdb::cache<int, int>>(capacity, std::move(evictor));
+    const auto cache = seastar::make_lw_shared<spiderdb::cache<int, int>>(capacity, std::move(evictor));
 
     // Generate access pattern
-    auto n_unique_items = capacity;
+    auto max_item = 256;
     auto access_pattern = seastar::make_lw_shared<item_list_t<int, int>>();
-    while (access_pattern->size() <= capacity) {
-        n_unique_items += capacity;
-        *access_pattern = generate_random_access_pattern<int, int>(n_unique_items, 0, n_unique_items);
+    size_t n_unique_items = 0;
+    while (n_unique_items <= capacity) {
+        max_item += 256;
+        *access_pattern = generate_random_data<int, int>(512, 0, max_item);
+        n_unique_items = remove_duplication(*access_pattern).size();
     }
     print("Access pattern", *access_pattern);
 
@@ -236,11 +238,58 @@ SPIDERDB_TEST_CASE(test_put_random_data_with_number_of_unique_items_greater_than
         return cache->put(key, value);
     }).then([cache, access_pattern, cache_history, capacity] {
         // Check result
-        auto expected_items = get_first_n_items(remove_duplication(reverse(*access_pattern)), capacity);
+        const auto& expected_items = get_first_n_items(remove_duplication(reverse(*access_pattern)), capacity);
         print("Cache items (actual)", cache->get_all_items());
         print("Cache items (expected)", expected_items);
         SPIDERDB_REQUIRE((is_equal<int, int>(cache->get_all_items(), expected_items)));
         SPIDERDB_REQUIRE(!cache_history->empty());
+    });
+}
+
+SPIDERDB_TEST_CASE(test_get) {
+    // Create cache
+    const size_t capacity = 256;
+    const auto cache_history = seastar::make_lw_shared<std::list<std::pair<int, int>>>();
+    auto evictor = [cache_history](const std::pair<int, int>& evicted_item) -> seastar::future<> {
+        cache_history->push_front(evicted_item);
+        return seastar::now();
+    };
+    const auto cache = seastar::make_lw_shared<spiderdb::cache<int, int>>(capacity, std::move(evictor));
+
+    // Generate cache data
+    const auto cache_size = 256;
+    assert(cache_size <= capacity);
+    const auto cache_data = seastar::make_lw_shared(generate_consecutive_data<int, int>(cache_size, 0));
+    print("Cache data", *cache_data);
+    *cache_data = reverse(*cache_data);
+
+    // Put items into cache
+    return seastar::do_for_each(*cache_data, [cache](const auto& item) {
+        const auto& key = item.first;
+        const auto& value = item.second;
+        return cache->put(key, value);
+    }).then([cache, cache_data, max_item{cache_size}] {
+        // Generate access pattern
+        const auto access_pattern = seastar::make_lw_shared(generate_random_data<int, int>(512, 0, max_item));
+        print("Access pattern", *access_pattern);
+
+        // Get items from cache
+        return seastar::do_for_each(*access_pattern, [cache](const auto& item) {
+            const auto& key = item.first;
+            const auto& value = item.second;
+            return cache->get(key).then_wrapped([cache, &key, &value](auto fut) {
+                SPIDERDB_REQUIRE_MESSAGE(!fut.failed(), "Key {} not found", key);
+                SPIDERDB_REQUIRE_MESSAGE(fut.get0() == value, "Key {} has a different value", key);
+            });
+        }).then([cache, access_pattern] {
+            // Check result
+            const auto n_unique_items = remove_duplication(*access_pattern).size();
+            const auto& actual_items = get_first_n_items(cache->get_all_items(), n_unique_items);
+            const auto& expected_items = remove_duplication(reverse(*access_pattern));
+            print("Cache items (actual)", actual_items);
+            print("Cache items (expected)", expected_items);
+            SPIDERDB_REQUIRE((is_equal<int, int>(actual_items, expected_items)));
+        });
     });
 }
 
